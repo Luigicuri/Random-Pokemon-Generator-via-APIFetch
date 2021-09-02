8719b28b698d4bece9508088593a5ee1111aa7d7
Luigi Curi <74516086+Luigicuri@users.noreply.github.com>
2021-09-02 17:50:19 -0300

    Update api.js
    Updated version

--- api.js
+++ api.js
@@ -1,27 +1,33 @@
-
 //Author: Luigi Curi
 
-function load() {
-    const numberOfPokemon = 898;
-    const id = Math.round(Math.random() * (numberOfPokemon - 1)) + 1;
-    let url = `https://pokeapi.co/api/v2/pokemon/${id}`; 
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
 
-            console.log(data);
-            let img = data['sprites']['front_default'];
-            document.getElementById('nome').innerHTML = data['name'];
-            document.getElementById('num').innerHTML = data['id'];
-            document.getElementById('type1').innerHTML = data?.types['0']?.type['name'];
-            document.getElementById('type2').innerHTML = data?.types['1']?.type['name'] || "-";
+function pokefetch() {
 
-            document.getElementById('pic').setAttribute('src', img);
+    function load() {
+        const numberOfPokemon = 898;
+        const id = Math.round(Math.random() * (numberOfPokemon - 1)) + 1;
+        let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        fetch(url)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
 
-        });
+                    console.log(data);
+                    let img = document.getElementById('pic').innerHTML = data['sprites']['front_default'];
+                    document.getElementById('nome').innerHTML = data['name'];
+                    document.getElementById('num').innerHTML = data['id'];
+                    document.getElementById('type1').innerHTML = data?.types['0']?.type['name'];
+                    document.getElementById('type2').innerHTML = data?.types['1']?.type['name'] || "-";
+                    document.getElementById('pic').setAttribute('src', img);
+                    //console.clear(data);
+                })
+        };
+
+     load();       
+                
+    
+    
 };
-
 
 
